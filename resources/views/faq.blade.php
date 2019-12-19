84ebae08f6a91f7b376ebf305628aeffc9325e6d
mmarquezchada <mmarquezchada@gmail.com>
2019-12-19 17:13:20 -0300

    va FAQ corregido

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -20,7 +20,7 @@
 
 <!--Font Awesome -->
 <script src="https://kit.fontawesome.com/c7094aec04.js"></script>
-<title>Formulario de contacto</title>
+<title>FAQ</title>
 
 </head>
 <body>
@@ -28,24 +28,145 @@
         <h1>Preguntas Frecuentes</h1>
 <div class="row">
         <div class="cuota col-md-12">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>
+            <br>
+            <br>
+            <strong> ¿COMO HAGO PARA COMPRAR? </strong>
+          </p>
+          <p>
+            1) ARMA TU PEDIDO
+            <br>
+            Elegí la cantidad de productos que quieras para armar tu pack, podés combinarlos como vos prefieras! Además, podés elegir un pack prediseñado por nosotros. Continuá la compra completando tus datos y, por último, nosotros la preparamos para vos. Recibila en tu casa y disfrutá de momentos únicos.
+          <br>
+          <br>
+
+             <strong>- ¿Hay un mínimo de productos para comprar?</strong>
+             <br>
+             No, podés elegir la cantidad que quieras y combinarlos como prefieras!. Aunque recorda que a mayor pedido el flete se bonifica. Ver Costos de Envío
+<br>
+          </p>
+          <br>
+          <p>
+            2) CONFIRMA LA COMPRA
+            <br>
+            Una vez que hayas completado tus selecciones, en tu carrito encontraras el detalle de tu compra. Recordá que podes combinar las diferentes formas de cajas<br>
+<br>
+             <strong>- ¿Tengo que registrarme para realizar una compra?</strong>
+             <br>
+             Sí, para realizar una compra es necesario que estés registrado. Podés hacerlo una vez que ya hayas llenado tu carrito! Y no te preocupes por el Spam somos respetuosos de la privacidad de tus datos, ver política de protección de los datos personales.
+
+Estamos comprometidos con la seguridad de las compras online, por lo que contamos con la tecnología SSL (Secure Sockets Layer) que asegura tanto la autenticidad de nuestro sitio como el cifrado de toda la información que nos entregás. Esta tecnología permite que tu información personal sea codificada para que no pueda ser leída cuando viaja a través de internet. Para garantizar la seguridad de los pagos usamos la tecnología de Mercado Pago.
+
+            <br>
+          </p>
+          <p>
+            <br>
+            3) REALIZA EL PAGO
+            <br>
+          Una vez acreditado el pago en nuestros sistemas procederemos a confeccionar el pedido para ser enviado a tu domicilio
+          <br>
+<br>
+             <strong>- ¿Puedo modificar mi compra una vez finalizada?</strong>
+             <br>
+                 No, una vez que tu orden está terminada ya no es posible sumarle más productos.
+
+            <br>
+          </p>
+          <p>
+            <br>
+            4) COORDINA EL ENVIO
+            <br>
+            Recibirás un email confirmando tu compra y tu numero de referencia, en el te pediremos que valides tus datos para coordinar tu pedido!
+            <br>
+            <br>
+             <strong>- ¿Puedo cambiar mi pedido después de haberlo pagado?</strong>
+             <br>
+             Digital Wine siempre tratará de ayudarte. Una vez que confirmamos tu pago enviamos tu pedido en forma inmediata, es posible que cuando reclames tu pedido este en camino. Comunícate lo antes posible para ver de qué manera podemos ayudarte.
+            <br>
+          </p>
+          <br>
         </div>
 
         <div class="preguntas col-md-12">
+
           <p>
-             <strong>¿Cuál es el minimo de compra?</strong>
+             <strong>DEVOLUCIONES</strong>
             <br>
-             El minimo de compra son 12 unidades.
-             Se pueden combinar diferentes modelos y talles</p>
+            Aceptaremos devoluciones en caso de roturas, defectos de embalaje, envíos equivocados o alguna otra razón de nuestra responsabilidad, siempre que te comuniques con nosotros dentro de los siguientes cinco(5) días hábiles contados desde la fecha de recepción del producto, para que podamos asistirte y coordinar el reemplazo sin costo alguno en el menor tiempo posible.
+          </p>
+          Las devoluciones se aceptarán en caso de:
+          <p>
+            <li>
+            Roturas
+            </li>
+
+            <li>
+            Defectos de embalaje
+            </li>
+            <li>
+            Envíos erróneos
+            </li>
+            <li>
+            Incidencias que dependan de nuestra responsabilidad
+            </li>
             <br>
+            Una vez aprobada la devolución, procederemos a reemplazar el o los productos correspondientes. Los gastos de envío correrán a cargo de Digital Wine. En caso de faltante de stock, procederemos con el reintegro del dinero en base al valor del o los productos, en la misma forma en que realizaste tu compra.
+            <br>
+            <br>
+          <p>
+             <strong>- Devoluciones sin causa</strong>
+            <br>
+
+Nos interesa que nuestros clientes estén felices con su compra, nuestra política es siempre tratar de ayudar, manteniendo una relación cordial y amable, que prospere con el tiempo.
+<br>
+Si tu compra fue entregada en perfecto estado, pero deseas ejercer tu derecho de desistimiento (derecho a la devolución del producto) posees un plazo de siete (7) días hábiles una vez recepcionada tu compra para comunicarte con nosotros.
+<br>
+Por devoluciones ajenas a Digital Wine, se procederá al reintegro del valor de la mercadería, en la medida que la misma se encuentre en perfecto estado. Los gastos de envío y retiro en base a la devolución correrán por cuenta del cliente.
+             
+<br>
+<br>
+<br>
+</p>
+        </div>
+
+        <div class="preguntas col-md-12">
+
+          <p>
+             <strong>¿Qué pasa si compro y luego no hay producto?</strong>
+            <br>
+            Digital Wine siempre intentará conseguirte el producto solicitado. De no haber o no poder conseguirlo, te informaremos qué demora puede llegar a tener y en ese caso el cliente podrá decidir si quiere seguir adelante con el pedido o si quiere desestimarlo sin ningún tipo de consecuencias.
+          </p>
+          
+          <p>
+          <p>
+             <strong>Si hago una compra, ¿cuánto tarda en llegar?</strong>
+            <br>
+            En general manejamos un promedio de entrega de 48 hs. Pero dependerá del producto solicitado. Al responder a tu pedido, te informaremos si hay o no demora, y SIEMPRE coordinaremos de manera personalizada el día y horario de entrega.</p>
+            
+          <p>
+          <p>
+             <strong>Al hacer una compra, ¿puedo pasar a retirar el producto por sus oficinas evitando así el pago del shipping?</strong>
+            <br>
+            Todo pedido se puede retirar en nuestras oficinas una vez que te avisemos que ya está disponible.
+             </p>
+          
+          <p>
+          <p>
+             <strong>¿Cómo puedo incluir publicidad en Digital Wine?</strong>
+            <br>
+            Si te interesa hacer publicidad en el sitio, envíanos un mail a info@digitalwine.com.ar.</p>
+          
           <p>
               <strong>¿Los precios incluyen IVA?</strong>
             <br>
-              No, los precios no incluyen IVA</p><br>
-          <p><strong>Ya hice mi compra, ¿Cómo sigue el proceso?</strong>
-            <br>
+              No, los precios no incluyen IVA</p>
+          <p>
+            <strong>Ya hice mi compra, ¿Cómo sigue el proceso?</strong>
+          <br>
             Una vez finalizado tu compra, vas a recibir un email de confirmación por la misma donde coordinaremos el envío de la mercadería</p>
-
+            <br>
+            <br>
+            <br>
 
         </div>
 
