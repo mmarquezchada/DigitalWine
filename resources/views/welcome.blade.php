a77f9e206212293b633c11d8ed8f2a9816ead563
mmarquezchada <mmarquezchada@gmail.com>
2019-12-20 16:10:22 -0300

    va descripcion vinos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -169,10 +169,11 @@
       <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
-          <a href="#">Item One</a>
+          <a href="#"> <strong>Zuccardi Concreto Malbec </strong></a>
         </h4>
-        <h5>$24.99</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+        <h5>$1677.00</h5>
+        <p class="card-text"><strong>Región: </strong> Mendoza</p>
+        <p><strong>Cepa: </strong> Malbec</p>
       </div>
       <div class="card-footer">
         <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -185,10 +186,11 @@
       <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
-          <a href="#">Item Two</a>
+          <a href="#"> <strong>Domingo Molina Malbec </strong></a>
         </h4>
-        <h5>$24.99</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+        <h5>$862.00</h5>
+        <p class="card-text"><strong>Región: </strong> Mendoza</p>
+        <p><strong>Cepa: </strong> Malbec</p>
       </div>
       <div class="card-footer">
         <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -201,10 +203,11 @@
       <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
-          <a href="#">Item Three</a>
+          <a href="#"> <strong>Don Miguel Escorihuela Gascon Malbec </strong></a>
         </h4>
-        <h5>$24.99</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+        <h5>$3539.00</h5>
+        <p class="card-text"><strong>Región: </strong> Mendoza</p>
+        <p><strong>Cepa: </strong> Cabernet Franc</p>
       </div>
       <div class="card-footer">
         <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -217,10 +220,11 @@
       <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
-          <a href="#">Item Four</a>
+          <a href="#"><strong>Gran Callejon Del Crimen Winemaker Selection Blend</strong></a>
         </h4>
-        <h5>$24.99</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+        <h5>$1314.00</h5>
+        <p class="card-text"><strong>Región: </strong> Mendoza</p>
+        <p><strong>Cepa: </strong> Blend</p>
       </div>
       <div class="card-footer">
         <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -233,10 +237,11 @@
       <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
-          <a href="#">Item Five</a>
+          <a href="#"><strong>Mosquita Muerta Blend De Blancas</strong></a>
         </h4>
-        <h5>$24.99</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+        <h5>$663.00</h5>
+        <p class="card-text"><strong>Región: </strong> Mendoza</p>
+        <p><strong>Cepa: </strong> Blend De Blancos</p>
       </div>
       <div class="card-footer">
         <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -249,10 +254,11 @@
       <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
-          <a href="#">Item Six</a>
+          <a href="#"><strong>The Apple Doedn't Fall Cabernet</strong></a>
         </h4>
-        <h5>$24.99</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+        <h5>$853.00</h5>
+        <p class="card-text"><strong>Región: </strong> Mendoza</p>
+        <p><strong>Cepa: </strong> Cabernet Sauvignon</p>
       </div>
       <div class="card-footer">
         <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
@@ -266,74 +272,6 @@
 </div>
      
         <!-- Fin de contenido -->
-    
           
     </body>
-
-    <!-- Footer -->
-    <footer class="bg-secondary">
-       <div class="container text-center">
-         <div class="row bg-secondary">
-           <div class="col-md-4 py-3">
-             <h3 class="text-white">Redes Sociales</h3>
-              <nav>
-                <ul class="social-header list-inline mb-0 my-3 text-center">
-                  <li class="list-inline-item">
-                    <a href="#">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-circle fa-stack-2x"></i>
-                            <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                          </span>
-                      </a>
-                  </li>
-                  <li class="list-inline-item">
-                    <a href="#">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-circle fa-stack-2x"></i>
-                            <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                          </span>
-                      </a>
-                  </li>
-                  <li class="list-inline-item">
-                    <a href="#">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-circle fa-stack-2x"></i>
-                            <i class="fab fa-youtube fa-stack-1x fa-inverse"></i>
-                          </span>
-                      </a>
-                  </li>
-                  <li class="list-inline-item">
-                    <a href="#">
-                        <span class="fa-stack fa-lg">
-                            <i class="fa fa-circle fa-stack-2x"></i>
-                            <i class="fab fa-instagram fa-stack-1x fa-inverse"></i>
-                          </span>
-                      </a>
-                  </li>
-                </ul>
-              </nav>
-           </div>
-           <div class="col-md-4 py-3">
-             <h3 class="text-white"><i class="fas fa-truck"></i> Envios Gratis</h3>
-             <p class="text-white">Compras de $1500 Rosario y alrededores</p>
-           </div>
-           <div class="col-md-4 py-3 payment-container">
-             <h3 class="text-white">Medios de pago</h3>
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/visa@2x.png" alt="" height="29">
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/mastercard@2x.png" alt="" height="29">
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/amex@2x.png" alt="" height="29">
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/ar/banelco@2x.png" alt="" height="29">
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/ar/cabal@2x.png" alt="" height="29">
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/ar/tarjeta-naranja@2x.png" alt="" height="29">
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/mercadopago@2x.png" alt="" height="29">
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/nativa@2x.png" alt="" height="29">
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/pagofacil@2x.png" alt="" height="29">
-             <img src="//d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/rapipago@2x.png" alt="" height="29">
-           </div>
-           <div class="col-md-12 text-center bg-secondary">
-               <p class="text-white pt-3">&copy;2019 - Todos los derechos reservados.</p>
-           </div>
-         </div>
-       </div>
-    </footer>
 </html>
